43deec9dc1ad95d54fe86f64554b656dd674bd4a
Jhonatan <jonathandiazp@gmail.com>
2022-10-08 14:05:57 -0400

    JAWSSB

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        host: 'localhost',
+    sequelize = new Sequelize(process.env.MYSQL_DBNAME, process.env.MYSQL_USER, process.env.MYSQL_KEY, {
+        host: process.env.MYSQL_HOST,
         dialect: 'mysql',
         port: 3306
     });
