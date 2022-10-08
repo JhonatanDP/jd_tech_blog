8ece2ba7639cf73b4e4e58884781e4564a199cbc
Jhonatan <jonathandiazp@gmail.com>
2022-10-08 15:05:39 -0400

    sqldialect

--- config/connection.js
+++ config/connection.js
@@ -24,7 +24,7 @@ if (process.env.JAWSDB_URL) {
 } else {
     sequelize = new Sequelize(process.env.MYSQL_DBNAME, process.env.MYSQL_USER, process.env.MYSQL_KEY, {
         host: process.env.MYSQL_HOST,
-        dialect: 'mysql',
+        dialect: process.env.MYSQL_DIALECT,
         port: 3306
     });
 }
