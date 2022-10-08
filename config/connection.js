3153101d7d8a5a1790b62b9be47020304bf6a1ea
Jhonatan <jonathandiazp@gmail.com>
2022-10-08 14:50:20 -0400

    freemysql

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,31 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE_NAME,
+  process.env.MYSQL_USERNAME,
+  process.env.MYSQL_PASSWORD,
+  {
+    dialect: process.env.MYSQL_DIALECT,
+    host: process.env.MYSQL_HOST,
+  }
+);
 
-// create connection to our db
-let sequelize;
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(process.env.MYSQL_DBNAME, process.env.MYSQL_USER, process.env.MYSQL_KEY, {
-        host: process.env.MYSQL_HOST,
-        dialect: 'mysql',
-        port: 3306
-    });
-}
 module.exports = sequelize;
+
+
+
+
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// // create connection to our db
+// let sequelize;
+// if (process.env.JAWSDB_URL) {
+//     sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//     sequelize = new Sequelize(process.env.MYSQL_DBNAME, process.env.MYSQL_USER, process.env.MYSQL_KEY, {
+//         host: process.env.MYSQL_HOST,
+//         dialect: 'mysql',
+//         port: 3306
+//     });
+// }
+// module.exports = sequelize;
